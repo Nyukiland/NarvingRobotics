9f5520a98e08d6c7bc8cb558819d77be7f939267
Nyukiland <roman211113@gmail.com>
2023-10-02 00:15:47 +0200

    rotation of the camera
    -rotation of the cam is complete
    - began set up for environment

--- script/RotateCam.gd
+++ script/RotateCam.gd
@@ -1,23 +1,25 @@
 extends MeshInstance3D
 
-var targetRot
-var currentRot
+
+var rot = 45.0
+var target_rot = 45.0  # The target rotation
+var rotation_speed = 10.0  # The speed at which the rotation occurs (degrees per second)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	currentRot = get_transform().basis.y
-	targetRot = get_transform().basis.y
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("RotateCamLeft"):
-		targetRot += 90
-		
+		target_rot += 90  
+
+	if Input.is_action_just_pressed("RotateCamRight"):
+		target_rot -= 90
+			
 	rotateToTarget()
 
-
-func rotateToTarget():
-	currentRot = get_transform().basis.y
-	if targetRot != currentRot:
-		rotate_object_local(Vector3.UP, 1)
+func rotateToTarget():	
+	var step = rotation_speed * get_process_delta_time()
+	rot = lerp(rot, target_rot, step)
+	rotation_degrees = Vector3(0, rot, 0)
