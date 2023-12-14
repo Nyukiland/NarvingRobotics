e7395e0b39f20c7f8df317aba94ee3867667078b
Nyukiland <roman211113@gmail.com>
2023-12-14 14:48:38 +0100

    look at need fix

--- script/player.gd
+++ script/player.gd
@@ -8,22 +8,32 @@ var animBallSpeed = 10.0
 var targetBallState = 0.0
 var ballState = 0.0
 
+var cam
+
+func _ready():
+	cam = get_node("/root/MainScene/PivotCam")
+
 
 func _physics_process(_delta):
-
+	
+	SPEED = get_meta("Speed")
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (cam.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+		
+		
+	look_at(direction, Vector3(0, 1, 0))
 	
+	#needed for the object to move
+	move_and_slide()
 	
 	#changement of ball state
 	if Input.is_action_just_pressed("ChangeBall"):
@@ -32,6 +42,7 @@ func _physics_process(_delta):
 		else:
 			targetBallState = 0.0
 	BallAnim()
+	
 	
 func BallAnim():
 	var step = animBallSpeed * get_process_delta_time()
